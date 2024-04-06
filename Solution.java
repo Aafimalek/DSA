3af5dd1d76769c4d63f20f1ca41329cc99b3f301
AAFI MALEK <aafimalek2032@gmail.com>
2024-04-06 21:15:17 +0530

    complted patterns

--- Solution.java
+++ Solution.java
@@ -307,10 +307,37 @@ public class Solution {
         }
     }
 
+    public static void nForest20(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void nForest21(int n) {
+        for (int i = 0; i < 2 * n - 1; i++) {
+            for (int j = 0; j < 2 * n - 1; j++) {
+                int top = i;
+                int left = j;
+                int right = (2 * n - 2) - j;
+                int down = (2 * n - 2) - i;
+                System.out.print(n-Math.min(Math.min(top, down), Math.min(left, right)));
+
+            }
+            System.out.print("\n");
+        }
+    }
+
     public static void main(String[] args) {
         Solution s1 = new Solution();
-        int n = 5;
-        s1.nForest19(n);
+        int n = 4;
+        s1.nForest21(n);
 
     }
 }
