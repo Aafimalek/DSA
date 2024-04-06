2c17956e9ca2b9027d940626d794f97292483166
AAFI MALEK <aafimalek2032@gmail.com>
2024-04-06 19:31:52 +0530

    more patterns

--- Solution.java
+++ Solution.java
@@ -107,12 +107,62 @@ public class Solution {
         }
     }
 
-    
+    public static void nForest9(int n) {
+        int start = 1;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                start = 1;
+            } else {
+                start = 0;
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(start);
+                start = 1 - start;
+            }
+
+            System.out.print("\n");
+        }
+    }
+
+    public static void nForest10(int n) {
+        int space = 2 * (n - 1);
+        for (int i = 0; i <= n; i++) {
+            // numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            // space
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+
+            // numbers
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.print("\n");
+            space -= 2;
+        }
+    }
+
+    public static void nForest11(int n) {
+       int num = 1;
+        for (int i = 1; i <= n; i++) {
+            for(int j = 1;j<=i;j++){
+                System.out.print(num+" ");
+                num+=1;
+            }
+           
+            System.out.print("\n");
+           
+        }
+    }
 
     public static void main(String[] args) {
         Solution s1 = new Solution();
         int n = 5;
-        s1.nForest8(n);
+        s1.nForest11(n);
 
     }
 }
